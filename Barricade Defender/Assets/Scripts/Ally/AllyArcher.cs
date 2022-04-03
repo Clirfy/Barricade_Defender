42127b45d1de672d9722cdc8f079b3062d3e71ee
Clirfy <w94.paluch@gmail.com>
2022-04-03 20:00:56 +0200

    - changed archer attack animation;

--- Barricade Defender/Assets/Scripts/Ally/AllyArcher.cs
+++ Barricade Defender/Assets/Scripts/Ally/AllyArcher.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class AllyArcher : Ally
 {
+    public float firstAttackDelay;
+
+    private float firstAttackDelayTimer;
+
     protected override void Update()
     {
         base.Update();
@@ -9,15 +13,21 @@ public class AllyArcher : Ally
         if (Time.time >= attackTimer && isAttacking)
         {
             animator.SetBool("isAttacking", true);
-            attackTimer = Time.time + attackDelay;
-            var bullet = Instantiate(ArrowPrefab, ShootPosition.transform.position, Quaternion.identity);
-            bullet.GetComponent<AllyBullet>().TargetPos = target;
-            bullet.GetComponent<AllyBullet>().Damage = Damage;
+            firstAttackDelayTimer -= Time.deltaTime;
+
+            if (firstAttackDelayTimer <= 0f)
+            {
+                attackTimer = Time.time + attackDelay;
+                var bullet = Instantiate(ArrowPrefab, ShootPosition.transform.position, Quaternion.identity);
+                bullet.GetComponent<AllyBullet>().TargetPos = target;
+                bullet.GetComponent<AllyBullet>().Damage = Damage;
+            }
         }
 
-        else
+        else if (isAttacking == false)
         {
             animator.SetBool("isAttacking", false);
+            firstAttackDelayTimer = firstAttackDelay;
         }
     }
 }
