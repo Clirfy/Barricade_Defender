91f0dd89e81d4e9d22077864572e515046cae891
Clirfy <w94.paluch@gmail.com>
2022-04-13 10:35:39 +0200

    -added ally druid 3rd talent tier logic

--- Barricade Defender/Assets/Scripts/Ally/Druid/AllyDruidTalents.cs
+++ Barricade Defender/Assets/Scripts/Ally/Druid/AllyDruidTalents.cs
@@ -6,8 +6,11 @@ public class AllyDruidTalents : MonoBehaviour
     public GameObject TalentPanel;
     public Button stTierButton;
     public Button[] ndTierButton;
+    public Button[] rdTierButton;
 
     private bool canUse2ndTier = false;
+    private bool canUse3rdTier = false;
+    private string rdSideTalent;
     private AllyDruid allyDruid;
     private AllyDruidUpgrades allyUpgrades;
 
@@ -28,6 +31,28 @@ public class AllyDruidTalents : MonoBehaviour
             foreach (Button item in ndTierButton)
             {
                 item.interactable = true;
+            }
+        }
+
+        if (canUse3rdTier == true && allyDruid.Level >= 15)
+        {
+            if (rdSideTalent == "left")
+            {
+                rdTierButton[0].interactable = true;
+                rdTierButton[1].interactable = true;
+
+                rdTierButton[2].interactable = false;
+            }
+            else if (rdSideTalent == "right")
+            {
+                rdTierButton[2].interactable = true;
+
+                rdTierButton[0].interactable = false;
+                rdTierButton[1].interactable = false;
+            }
+            else
+            {
+                Debug.LogWarning("incorrect string passed when advancing to third talent tier");
             }
         }
     }
@@ -51,16 +76,23 @@ public class AllyDruidTalents : MonoBehaviour
         canUse2ndTier = true;
     }
 
+    private string Return3rdSideTalent(string side)
+    {
+        return rdSideTalent = side;
+    }
+
     public void Click2ndTier(int talent)
     {
         switch (talent)
         {
             case 0:
                 allyDruid.SpellDmgModifier = 8;
+                Return3rdSideTalent("left");
                 break;
 
             case 1:
                 allyDruid.SkillTargetCount = 5;
+                Return3rdSideTalent("right");
                 break;
 
             default:
@@ -68,8 +100,39 @@ public class AllyDruidTalents : MonoBehaviour
         }
 
         canUse2ndTier = false;
+        canUse3rdTier = true;
 
         foreach (Button item in ndTierButton)
+        {
+            item.interactable = false;
+        }
+    }
+
+    public void Click3rdTier(int talent)
+    {
+        switch (talent)
+        {
+            case 0:
+                allyDruid.SpellDmgModifier = 16;
+                break;
+
+            case 1:
+                allyDruid.SpellDmgModifier = 10;
+                allyDruid.SkillTargetCount = 4;
+                break;
+
+            case 2:
+
+                allyDruid.SkillTargetCount = 8;
+                break;
+
+            default:
+                break;
+        }
+
+        canUse3rdTier = false;
+
+        foreach (Button item in rdTierButton)
         {
             item.interactable = false;
         }
