d233814ec1a17f3f6c06d8ca2733666cee8829e4
Clirfy <w94.paluch@gmail.com>
2022-04-14 18:48:02 +0200

    -working on UI: added MainMenu scene

--- Barricade Defender/Assets/Scripts/GameManager.cs
+++ Barricade Defender/Assets/Scripts/GameManager.cs
@@ -1,6 +1,41 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public class GameManager : MonoBehaviour
+public class GameManager : SingletonMonoBehaviour<GameManager>
 {
+    public UnityEvent OnGamePaused;
+    private GameObject Player;
+    private SceneLoader sceneLoader;
 
+    protected override void Awake()
+    {
+        DontDestroy();
+    }
+
+    private void Update()
+    {
+        PauseGame();
+    }
+
+    private void DontDestroy()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("GameManager");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void PauseGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0f;
+            Player.GetComponent<PlayerController>().enabled = false;
+            OnGamePaused.Invoke();
+        }
+    }
 }
